2ce13a3a9ce677e5f15751fe103ad78bb603d5e5
hajun1601 <ngobaolong.vn@gmail.com>
2026-09-07 21:35:06 +0700

    cập nhật ngày thứ 1

---
+++ Bai1/bai4.cpp
@@ -0,0 +1,12 @@
+// Làm chương trình thuật toán cơ bản + - * /
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "2006 + 2026 = " << 2006 + 2026 << endl;
+    cout << "2006 - 2026 = " << 2006 - 2026 << endl;
+    cout << "2006 * 2026 = " << 2006 * 2026 << endl;
+    cout << "2006 / 2026 = " << 2006 / 2026 << endl;
+    return 0;
+}
