5830c5955af9e77f6d9286e7659e05021987fe49
hajun1601 <ngobaolong.vn@gmail.com>
2026-09-07 23:20:02 +0700

    cap nhat 9/7/2026 11:19

---
+++ Bai1/bai5.cpp
@@ -0,0 +1,13 @@
+// viết một chương trình khai báo biến a,b in ra kết quả.
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 100, b = 200;
+    cout << "A = 100, B = 200 \n tính + - * / " << endl;
+    cout << " A + B = " << a + b << endl;
+    cout << " A - B = " << a - b << endl;
+    cout << " A * B = " << a * b << endl;
+    cout << " A / B = " << a / b << endl;
+}
---
+++ Bai1/bt6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string name = "Codelearn";
+    int dob = 2019;
+    cout << "Name: " << name << endl;
+    cout << "Date of birth: " << dob;
+    return 0;
+}
