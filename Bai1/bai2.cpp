d47533556eae144271b726005e3b403979afbed5
hajun1601 <ngobaolong.vn@gmail.com>
2026-09-07 21:10:54 +0700

    Bài tập thực hiện học lại code c++ trong 1 tuần

---
+++ Bai1/bai2.cpp
@@ -0,0 +1,12 @@
+// Tự tạo 1 chương trình giới thiệu về bản thân.
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Xin chào tôi tên : NGÔ BẢO LONG";
+    cout<<"Tôi là sinh viên trường : Đại Học Công Nghiệp Thái Nguyên TNUT";
+    cout<<"Hiện tại tôi đang phải học lại những thứ cơ bản nhất mà tôi đã không học";
+    cout<<"Tôi hiện đang trong quá trình học lại rèn lại bản thân từ gốc kể từ bây giờ.";
+}
+// Tôi quên mất cách xuống dòng rồi.
